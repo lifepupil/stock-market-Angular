7bfd31172047a7c4a8463455774da8171f6bd781
Christian Richard <lifepupil@gmail.com>
2015-04-23 00:11:05 -0700

    adding stock to a portfolio in process

--- client/index.js
+++ client/index.js
@@ -17,9 +17,17 @@ angular.module('stock-market-Angular', ['firebase'])
   var afPortfolios = $firebaseArray(fbPortfolios);
   $scope.portfolios = afPortfolios;
 
+  // $scope.portfolios.$loaded().then(function(){
+  //   var fbPortfolio = $scope.fbPortfolios.child('portfolio');
+  //   var afPortfolio = $firebaseArray(fbPortfolio);
+  //   $scope.portfolio = afPortfolio;
+  // });
+
   $scope.addPortfolio = function() {
     // console.log($scope.portfolio);
+
     $scope.portfolios.$add($scope.portfolio);
+    $scope.portfolio = {};
 
 
   };
@@ -37,8 +45,38 @@ angular.module('stock-market-Angular', ['firebase'])
     var upperCaseSymbol = angular.uppercase($scope.symbol);
     $http.jsonp('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol='+upperCaseSymbol+'&callback=JSON_CALLBACK').then(function(response) {
       console.info('quote', response.data.LastPrice);
+      $scope.lastPrice = response.data.LastPrice;
+      console.log();
+      $scope.totalPrice = $scope.lastPrice * $scope.shareNum;
     });
   };
+
+  $scope.buyStock = function(){
+    if ($scope.portfolio.portName !== undefined && $scope.user.balance >= ($scope.user.balance - $scope.totalPrice)){
+      $scope.user.balance -= $scope.totalPrice;
+      var stock = {
+        symbol: $scope.symbol,
+        cost: $scope.totalPrice,
+        shares: $scope.shareNum
+      }
+
+      console.log($scope.portfolios[$scope.portfolio]);
+      $scope.portfolios[0].stock = stock;
+      // $scope.portfolios[0].$scope.symbol = stock;
+      $scope.portfolios.$save(0);
+      // console.log($scope.portfolios[0]);
+      // $scope.portfolio.stock = stock;
+      // // console.log($scope.portfolio.stock);
+      // // console.log($scope.portfolio);
+      // $scope.portfolios.$save($scope.portfolio);
+      // // $scope.user.$save();
+    }
+
+    // find portfolio children and find way to add to desired one
+    // console.log($scope.portfolio.child);
+    // console.log();
+    // console.log();
+  }
 
 
 
